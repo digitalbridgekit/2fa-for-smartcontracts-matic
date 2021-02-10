e95f64af1310013a3bd3e3cc3999072be72e83f0
apronotti <apronotti@sismatech.com.ar>
2021-02-10 09:59:22 -0300

    Change the way to call requestGAPINCheck.

--- contract/GAPINCheck-mumbai-maticnetwork.sol
+++ contract/GAPINCheck-mumbai-maticnetwork.sol
@@ -5,11 +5,11 @@ import "https://github.com/smartcontractkit/chainlink/evm-contracts/src/v0.4/ven
 
 contract GoogleAuthenticatorPINCheck is ChainlinkClient, Ownable {
   uint256 constant private ORACLE_PAYMENT = 10 ** 16;
-  bytes32  constant private JOBID = "612cd0ec646f46669613fa7bf7187f5e";
-  address constant private ORACLE_ADDRESS = 0x10d914A0586E527247C9530A899D74dC189Dbd80;
-  
+  bytes32  constant private JOBID = "f82dfad608254bc7a36364f317e47a4d";
+  address constant private ORACLE_ADDRESS = 0xA244B30a48559d16078BB151f342d3f12219142F;
+
   bool public currentPermission;
-  
+
   event RequestGAPINCheckFulfilled(
     bytes32 indexed requestId,
     bool indexed allowed
@@ -20,18 +20,18 @@ contract GoogleAuthenticatorPINCheck is ChainlinkClient, Ownable {
     currentPermission = false;
   }
 
-  function requestGAPINCheck(string memory _customerId, string memory _pin)
+  function requestGAPINCheck(string memory _customerId, int256 _hashedpin)
     public
   {
     Chainlink.Request memory req = buildChainlinkRequest(JOBID, address(this), this.fulfillGAPINCheck.selector);
-    
+
     req.add("customerid", _customerId);
-    req.addInt("hashedpin", int256(bytes6(keccak256(bytes(_pin)))));
+    req.addInt("hashedpin", _hashedpin);
     req.add("path","result");
-    
+
     sendChainlinkRequestTo(ORACLE_ADDRESS, req, ORACLE_PAYMENT);
   }
-
+  
   function fulfillGAPINCheck(bytes32 _requestId, bool _allowed)
     public
     recordChainlinkFulfillment(_requestId)
@@ -45,14 +45,14 @@ contract GoogleAuthenticatorPINCheck is ChainlinkClient, Ownable {
         //currentPermission = false;
     }
   }
-
+  
   function withdrawLink() public onlyOwner {
     LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
     require(link.transfer(msg.sender, link.balanceOf(address(this))), "Unable to transfer");
   }
-  
+
   function getChainlinkToken() public view returns (address) {
     return chainlinkTokenAddress();
   }
-  
+
 }
